ea87bfcbaae45b7afa48f8fb9bcc0a2e07c82458
irorere juliet <irorerejuliet@gmail.com>
2025-08-10 05:04:18 +0100

    created empty cart section

--- src/Components/Pages/Cart.jsx
+++ src/Components/Pages/Cart.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { useSelector } from "react-redux"
+
+useSelector
 
 const Cart = () => {
+  const cart = useSelector(state => state.cart)
   return (
     <div>
-      
+      {cart.products.length > 0 ? (
+        <div>
+          <h3>SHOPPING CART</h3>
+          <div>
+            <div>
+              <p>PRODUCTS</p>
+              <div>
+                <p>Price</p>
+                <p>PRICE</p>
+                <p>SUBTOTAL</p>
+                <p>REMOVE</p>
+                </div>
+                <div>
+                {cart.products.map((product) =>(
+                  <div>
+                    <div>
+                      <img src={product.image} alt="" />
+                      <div><h3>{product.name}</h3></div>
+                    </div>
+                    <div>
+                      <p>${product.price}</p>
+                    </div>
+                  </div>
+                ))}
+                </div>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <img src="/Images/emptyCart.jpg" alt="emptyCart" />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Cart
