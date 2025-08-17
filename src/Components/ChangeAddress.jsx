85896f51daffdeea2b7dce0d465283bd6de71755
irorere juliet <irorerejuliet@gmail.com>
2025-08-17 22:25:17 +0100

    created, modal section

---
+++ src/Components/ChangeAddress.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChangeAddress = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChangeAddress
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div>
+      div\
+      
+    </div>
+  )
+}
+
+export default Modal
