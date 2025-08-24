c31355cef8cb91d8ea54f05bfe12a68c5ca2cf7c
irorere juliet <irorerejuliet@gmail.com>
2025-08-24 06:53:59 +0100

    product details page completed

--- src/Components/Pages/ProductDetails.jsx
+++ src/Components/Pages/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { FaQuestion } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 
@@ -16,23 +17,47 @@ const ProductDetails = () => {
     if (!product) return <div>Loading....</div>;
 
   return (
-    <div className='container mx-auto py-8 px-4 md:px-16 lg:px-24'>
-        <div className='flex flex-col md:flex-row gap-x-16'>
-            {/* product image */}
-            <div className='md:w-1/2 py-4 shadow-md md:px-8 h-96 flex justify-center'>
-                <img src={product.image} alt={product.name}  className='h-full'/>
-            </div>
+    <div className="container mx-auto py-8 px-4 md:px-16 lg:px-24">
+      <div className="flex flex-col md:flex-row gap-x-16">
+        {/* product image */}
+        <div className="md:w-1/2 py-4 shadow-md md:px-8 h-96 flex justify-center">
+          <img src={product.image} alt={product.name} className="h-full" />
+        </div>
         {/* product information */}
-        <div className='md:w-1/2 p-4 shadow-md md:p-16 flex flex-col items-center gap-y-2'>
-        <h2 className='text-3xl font-semibold mb-2'>{product.name}</h2>
-        <p className='text-xl font-semibold text-gray-800 mb-4'>${product.price}</p>
-        <div className='flex items-center mb-4 gap-x-2'>
+        <div className="md:w-1/2 p-4 shadow-md md:p-16 flex flex-col items-center gap-y-2">
+          <h2 className="text-3xl font-semibold mb-2">{product.name}</h2>
+          <p className="text-xl font-semibold text-gray-800 mb-4">
+            ${product.price}
+          </p>
+          <div className="flex items-center mb-4 gap-x-2">
             {/* <label htmlFor="quantity" className='mr-2'>Quantity:</label> */}
-            <input  id="" type='number' id/>
+            <input
+              type="number"
+              id="quantity"
+              min="1"
+              className="border p-1 w-16"
+            />
 
-      
+            <button className="flex flex-col gap-y-4 mt-4">Add to Cart</button>
+            <p className="flex items-center">
+              {" "}
+              <FaQuestion className="mr-1" />
+              Delivery & Return
+            </p>
+            <p className="flex items-center">
+              {" "}
+              <FaQuestion className="mr-1" />
+              Ask a question
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-8">
+        <h3 className="text-xl font-bold mb-2">Product Descriprition</h3>
+        <p>Product description</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProductDetails
